2c8afdcb20af508ee9338802b07c9b7df8a656cf
Chandru Tharmalingom Santhi <chandru.ts@InTechHub.com>
2024-08-13 16:30:03 +0530

    Folder path changed to inside the project

--- SF_WebsiteToPdf/Program.cs
+++ SF_WebsiteToPdf/Program.cs
@@ -7,17 +7,20 @@ Console.WriteLine("Hello, World!");
 
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzQwMDY4MEAzMjM2MmUzMDJlMzBEaXFOMHhrRy9aWEttTVoyVnMxZ2dFUFI3ZTBZbENDK2FxS21KdmY3aTBrPQ==");
 
-string filePath = "C:\\Users\\chandru.ts\\Desktop\\Urls.txt";
+string projectDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+string inputFilePath = Path.Combine(projectDirectory, "InputFiles\\Urls.txt");
+string outputPath = Path.Combine(projectDirectory, "OutputFiles");
+
 int viewportWidth = 1366;
 int viewportHeight = 0;
 
-if (!File.Exists(filePath))
+if (!File.Exists(inputFilePath))
 {
     Console.WriteLine("File not found.");
     return;
 }
 
-string[] urls = File.ReadAllLines(filePath);
+string[] urls = File.ReadAllLines(inputFilePath);
 
 if(!urls.Any())
 {
@@ -28,7 +31,7 @@ if(!urls.Any())
 BlinkConverterSettings settings = new BlinkConverterSettings();
 settings.ViewPortSize = new Size(viewportWidth, viewportHeight);
 settings.Margin.Top = 0;
-settings.AdditionalDelay = 3000;
+settings.AdditionalDelay = 5000;
 settings.MediaType = MediaType.Screen;
 settings.Css = CustomStyle.customCss;
 
@@ -43,7 +46,7 @@ foreach (var url in urls)
     {
         PdfDocument document = htmlConverter.Convert(url);
 
-        using (FileStream stream = new FileStream($"C:\\Users\\chandru.ts\\Downloads\\outputFile{index++}.pdf", FileMode.Create, FileAccess.Write))
+        using (FileStream stream = new FileStream($"{outputPath}\\outputFile{index++}.pdf", FileMode.Create, FileAccess.Write))
         {
             document.Save(stream);
         }
